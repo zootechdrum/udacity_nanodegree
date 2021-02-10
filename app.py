f7196f9670022d963331d239968e2b936dcdc134
Cesar Gomez <zootechdrum@gmail.com>
2021-02-09 19:17:00 -0800

    added migration files to project

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import sys
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zootechdrum@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
-db.create_all()
 
 @app.route('/')
 def index():
@@ -23,7 +29,23 @@ def index():
 
 
 @app.route('/todos/create', methods=['POST'])
-def create():
-    desc = request.form.get('description')
-    print(desc)
-    return render_template('index.html')
+def create_todo():
+    error = False
+    body = {}
+    try: 
+    # can optionaly take 2 arguments if nothing is submitted in the form
+        desc = request.get_json()['description']
+        todo = Todo(description=desc)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    if  error:
+        abort(400)
+    else:    
+        return jsonify(body)
