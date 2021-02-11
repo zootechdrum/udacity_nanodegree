5fae6ed72eea26ab7cb524cc5c7d9f93ce13b952
Cesar Gomez <zootechdrum@gmail.com>
2021-02-10 16:45:49 -0800

    added new migration file. created new model for creation of seperate todolist

--- app.py
+++ app.py
@@ -17,15 +17,26 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'),nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+class TodoList(db.Model):
+    __tablename__= 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo',backref='list', lazy=True)
 
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id'))
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
+
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -50,9 +61,10 @@ def create_todo():
     else:    
         return jsonify(body)
 
-
-@app.route('/todos/<todo_id>set-completed', methods=['POST'])
+# This route updated the todo list item
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
+    print(todo_id)
     try:
         completed = request.get_json()['completed']
         todo = Todo.query.get(todo_id)
@@ -62,3 +74,16 @@ def set_completed_todo(todo_id):
     finally:
         db.session.close()
     return redirect(url_for('index'))
+
+
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+    print(todo_id)
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success' : True})
